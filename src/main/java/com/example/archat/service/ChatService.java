9d93939f00bb333ad4f0e4f46706865e30f9af44
a1l1ke <onetenthplease@gmail.com>
2026-07-02 10:51:14 +0900

    docs: ChatService 의존성 주입 관련 설명 주석 추가

--- src/main/java/com/example/archat/service/ChatService.java
+++ src/main/java/com/example/archat/service/ChatService.java
@@ -8,6 +8,7 @@ public class ChatService {
     public ChatService() {
         // ChatService - InMemoryChatRepository
         // 생성자에서 엮여있긴 함. -> chatRepository을 호출하는 메서드는 InMemoryChatRepository 알아야함?
+        // No. 만약 SupabaseChatRepository로 바꾼다면 생성자에서 진행하는 의존성 주입을 바꿔주기만 하면 OK
         this.chatRepository = InMemoryChatRepository.getInstance();
     }
 }

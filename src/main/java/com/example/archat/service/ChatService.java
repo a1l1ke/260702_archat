f415139bf2ecfe1e159027882e17cc73ccc94a93
a1l1ke <onetenthplease@gmail.com>
2026-07-02 11:09:39 +0900

    feat: ChatService 싱글톤 적용 및 채팅 내역 조회 기능 추가

--- src/main/java/com/example/archat/service/ChatService.java
+++ src/main/java/com/example/archat/service/ChatService.java
@@ -5,14 +5,23 @@ import com.example.archat.model.repository.ChatRepository;
 import com.example.archat.model.repository.InMemoryChatRepository;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 public class ChatService {
+
     private final ChatRepository chatRepository;
-    public ChatService() {
+    // 1. 생성자를 private으로 변경
+    private ChatService() {
         // ChatService - InMemoryChatRepository
         // 생성자에서 엮여있긴 함. -> chatRepository을 호출하는 메서드는 InMemoryChatRepository 알아야함?
         // No. 만약 SupabaseChatRepository로 바꾼다면 생성자에서 진행하는 의존성 주입을 바꿔주기만 하면 OK
         this.chatRepository = InMemoryChatRepository.getInstance();
+    }
+    // 2. instance를 static에 등록
+    private static final ChatService instance = new ChatService();
+    // 3. getInstance() 메서드 만들기 (나중에 상위에서 쓸)
+    public static ChatService getInstance() {
+        return instance;
     }
 
     public void sendMessage(Chat chat) {
@@ -33,6 +42,10 @@ public class ChatService {
 
     private String useAI(Chat chat) {
         return "%s라고 하셨네요.".formatted(chat.message());
+    }
+
+    public List<Chat> readHistory(String userId) {
+        return chatRepository.findAllByUserId(userId);
     }
 
 }

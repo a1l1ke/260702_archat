18a11062e40daf2876f3e1d033cb370f944d7505
a1l1ke <onetenthplease@gmail.com>
2026-07-02 15:08:58 +0900

    refactor: Gemini API 호출 로직을 GenAIChatProvider 어댑터로 완전 이관 및 ChatProvider 오버로딩 구현

--- src/main/java/com/example/archat/application/service/GeminiChatService.java
+++ src/main/java/com/example/archat/application/service/GeminiChatService.java
@@ -5,12 +5,7 @@ import com.example.archat.domain.model.Chat;
 import com.example.archat.domain.repository.ChatRepository;
 import com.example.archat.domain.service.ChatService;
 import com.example.archat.infrastructure.api.GenAIChatProvider;
-import com.example.archat.infrastructure.api.GenAIConfig;
 import com.example.archat.infrastructure.repository.InMemoryChatRepository;
-import com.google.genai.Client;
-import com.google.genai.types.Content;
-import com.google.genai.types.GenerateContentResponse;
-import com.google.genai.types.Part;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -24,7 +19,8 @@ public class GeminiChatService implements ChatService {
     public void save(Chat chat) {
         chatRepository.save(chat);
 //        String aiResponse = useAI(chat);
-        String aiResponse = chatProvider.useAI(chat);
+        List<Chat> history = chatRepository.findAllByUserId(chat.userId());
+        String aiResponse = chatProvider.useAI(chat, history);
         Chat aiChat = new Chat(
                 aiResponse,
                 "AI",
@@ -38,26 +34,6 @@ public class GeminiChatService implements ChatService {
     @Override
     public List<Chat> findAllByUserId(String userId) {
         return chatRepository.findAllByUserId(userId);
-    }
-
-    private String useAI(Chat chat) {
-        List<Chat> history = chatRepository.findAllByUserId(chat.userId());
-        List<Content> contents = history.stream()
-                .map((c) -> Content.builder()
-                        .role(chat.owner().equals("USER") ? "user" : "model")
-                        .parts(Part.builder().text(c.message()).build())
-                        .build())
-                .toList();
-        try (Client client = GenAIConfig.getClient()) {
-            GenerateContentResponse response = client.models.generateContent(
-                    chat.model(),
-                    contents,
-                    GenAIConfig.getGenerateContentConfig());
-            return response.text();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "문제가 생겼어요 : %s".formatted(e.getMessage());
-        }
     }
 
     // 싱글톤 등록
